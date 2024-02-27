34c5ee4ecb8af5600ee274154122a18c0a8bf66a
Gwangmu Lee <gwangmu.lee@epfl.ch>
2024-02-27 15:02:11 +0100

    fixed setup script

--- setup.sh
+++ setup.sh
@@ -2,8 +2,11 @@
 
 # Install LLVM 15.0.5
 
-LLVM_TAR_NAME=clang+llvm-15.0.5-x86_64-linux-gnu-ubuntu-18.04.tar.xz
-LLVM_URL=https://github.com/llvm/llvm-project/releases/download/llvmorg-15.0.5/$LLVM_TAR_NAME
+LLVM_URL=https://github.com/llvm/llvm-project/releases/download/llvmorg-15.0.5/clang+llvm-15.0.5-x86_64-linux-gnu-ubuntu-18.04.tar.xz
+LLVM_TAR_NAME=${LLVM_URL##*/}
+
+echo $LLVM_TAR_NAME
+exit
 
 wget $LLVM_URL
 tar -xvf $LLVM_TAR_NAME llvm
