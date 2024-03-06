8e77de70196c9c32f2136be329079e39729aae52
Gwangmu Lee <gwangmu@epfl.ch>
2024-03-06 23:52:26 +0100

    paper: discussion: quality metric for general fuzzing (revise./cov-to-fig.py lscov.csv) --> replacing 'completeness of targeted fuzzing'

--- lscov/cov-to-fig.py
+++ lscov/cov-to-fig.py
@@ -51,67 +51,96 @@ cov_unit_str = ""
 if (cov_unit_pow10 > 0):
     cov_unit_str = " (x$10^{}$)".format(cov_unit_pow10)
 
-# FIXME: there must be a better algorithm for this kind of stuff.
-time_unit = 1
-_max_time = data['Time'][-1]
-while (True):
-    if (time_unit == 1):
-        next_time_unit = 10
-    elif (time_unit == 10):
-        next_time_unit = 60
-    elif (round(math.log(time_unit // 6, 10), 3).is_integer()):
-        next_time_unit = (time_unit // 2) * 3
-    elif (round(math.log(time_unit // 9, 10), 3).is_integer()):
-        next_time_unit = (time_unit // 3) * 4 
-    elif (round(math.log(time_unit // 12, 10), 3).is_integer()):
-        next_time_unit = (time_unit // 4) * 5
-    elif (round(math.log(time_unit // 15, 10), 3).is_integer()):
-        next_time_unit = (time_unit // 5) * 6 
-    elif (round(math.log(time_unit // 18, 10), 3).is_integer()):
-        next_time_unit = (time_unit // 3) * 4 
-    elif (round(math.log(time_unit // 24, 10), 3).is_integer()):
-        next_time_unit = (time_unit // 4) * 5 
-    elif (round(math.log(time_unit // 30, 10), 3).is_integer()):
-        next_time_unit = (time_unit // 2) * 3
-    elif (round(math.log(time_unit // 45, 10), 3).is_integer()):
-        next_time_unit = (time_unit // 3) * 4 
-    _num_time_ticks = _max_time // next_time_unit
-    if (_num_time_ticks <= 3): break
-    time_unit = next_time_unit
+# Decide time unit.
 
-time_fmt = ticker.FuncFormatter(lambda ms, x: int(ms / 60))
+# FIXME: there must be a better algorithm for this kind of stuff.
+time_unit_in_str = 'sec'
+time_unit_in_fmt = 1
+time_unit_in_sec = 1
+_max_time = data['Time'][-1]
+
+if (_max_time > 4*60*60):
+    time_unit_in_str = 'hr'
+    time_unit_in_fmt = 3600
+elif (_max_time > 4*60):
+    time_unit_in_str = 'min'
+    time_unit_in_fmt = 60
+
+while (True):
+    if (time_unit_in_str == 'sec'):
+        if (time_unit_in_sec == 1):
+            next_time_unit_in_sec = 2
+        elif (time_unit_in_sec == 2):
+            next_time_unit_in_sec = 5
+        elif (time_unit_in_sec == 5):
+            next_time_unit_in_sec = 10 
+        elif (round(math.log(time_unit_in_sec, 10), 3).is_integer()):
+            next_time_unit_in_sec = time_unit_in_sec * 2
+        elif (round(math.log(time_unit_in_sec // 2, 10), 3).is_integer()):
+            next_time_unit_in_sec = (time_unit_in_sec // 2) * 5
+        elif (round(math.log(time_unit_in_sec // 5, 10), 3).is_integer()):
+            next_time_unit_in_sec = time_unit_in_sec * 2 
+    elif (time_unit_in_str == 'min'):
+        if (time_unit_in_sec == 1):
+            next_time_unit_in_sec = 60
+        elif (round(math.log(time_unit_in_sec // 6, 10), 3).is_integer()):
+            next_time_unit_in_sec = (time_unit_in_sec // 2) * 3
+        elif (round(math.log(time_unit_in_sec // 9, 10), 3).is_integer()):
+            next_time_unit_in_sec = (time_unit_in_sec // 3) * 4 
+        elif (round(math.log(time_unit_in_sec // 12, 10), 3).is_integer()):
+            next_time_unit_in_sec = (time_unit_in_sec // 4) * 5
+        elif (round(math.log(time_unit_in_sec // 15, 10), 3).is_integer()):
+            next_time_unit_in_sec = (time_unit_in_sec // 5) * 6 
+        elif (round(math.log(time_unit_in_sec // 18, 10), 3).is_integer()):
+            next_time_unit_in_sec = (time_unit_in_sec // 3) * 4 
+        elif (round(math.log(time_unit_in_sec // 24, 10), 3).is_integer()):
+            next_time_unit_in_sec = (time_unit_in_sec // 4) * 5 
+        elif (round(math.log(time_unit_in_sec // 30, 10), 3).is_integer()):
+            next_time_unit_in_sec = (time_unit_in_sec // 2) * 3
+        elif (round(math.log(time_unit_in_sec // 45, 10), 3).is_integer()):
+            next_time_unit_in_sec = (time_unit_in_sec // 3) * 4 
+    elif (time_unit_in_str == 'hr'):
+        if (time_unit_in_sec == 1):
+            next_time_unit_in_sec = 3600 
+        else:
+            next_time_unit_in_sec = ((time_unit_in_sec // 3600) + 1) * 3600
+    _num_time_ticks = _max_time // next_time_unit_in_sec
+    if (_num_time_ticks <= 3): break
+    time_unit_in_sec = next_time_unit_in_sec
+
+time_fmt = ticker.FuncFormatter(lambda ms, x: int(ms / time_unit_in_fmt))
 cov_fmt = ticker.FuncFormatter(lambda ms, x: int(ms / (10 ** cov_unit_pow10)))
 
 ax[0].plot(data['Time'], data['Coverage'], color='C0')
 ax[0].fill_between(data['Time'], data['Coverage'], color='C0', alpha=0.2)
-ax[0].set_xlabel("Time (min)")
+ax[0].set_xlabel("Time ({})".format(time_unit_in_str))
 ax[0].set_ylabel("Logic State Coverage{}".format(cov_unit_str))
 ax[0].set_xlim([data['Time'][0], data['Time'][-1]])
 ax[0].set_ylim(bottom=0)
 ax[0].xaxis.set_major_formatter(time_fmt)
 ax[0].yaxis.set_major_formatter(cov_fmt)
-ax[0].xaxis.set_major_locator(ticker.MultipleLocator(time_unit))
+ax[0].xaxis.set_major_locator(ticker.MultipleLocator(time_unit_in_sec))
 
 ax[1].plot(data['Time'], data['RateS(ins)'], color='C1', alpha=0.5)
 ax[1].plot(data['Time'], data['RateS(avg)'], color='C1', linewidth=2)
-ax[1].set_xlabel("Time (min)")
+ax[1].set_xlabel("Time ({})".format(time_unit_in_str))
 ax[1].set_ylabel("New Coverage (/sec)")
 ax[1].set_xlim([data['Time'][0], data['Time'][-1]])
 ax[1].set_ylim(bottom=0)
 ax[1].legend(["Inst", "Avg"], loc="upper right")
 ax[1].xaxis.set_major_formatter(time_fmt)
-ax[1].xaxis.set_major_locator(ticker.MultipleLocator(time_unit))
+ax[1].xaxis.set_major_locator(ticker.MultipleLocator(time_unit_in_sec))
 
 ax[2].plot(data['Time'], data['RateE(ins)'], color='C2', alpha=0.5)
 ax[2].plot(data['Time'], data['RateE(avg)'], color='C2', linewidth=2)
-ax[2].set_xlabel("Time (min)")
+ax[2].set_xlabel("Time ({})".format(time_unit_in_str))
 ax[2].set_ylabel("New Coverage (/exec)")
 ax[2].set_xlim([data['Time'][0], data['Time'][-1]])
 ax[2].set_ylim([0, 100])
 ax[2].legend(["Inst", "Avg"], loc="upper right")
 ax[2].xaxis.set_major_formatter(time_fmt)
 ax[2].yaxis.set_major_formatter('{x:.0f}%')
-ax[2].xaxis.set_major_locator(ticker.MultipleLocator(time_unit))
+ax[2].xaxis.set_major_locator(ticker.MultipleLocator(time_unit_in_sec))
 
 # Save.
 
